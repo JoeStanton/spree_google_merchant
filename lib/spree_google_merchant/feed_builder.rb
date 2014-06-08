e598e06a1c3dd889a4f9cc822168ce96ff9e4f8e
Joe Stanton <joe.stanton@red-badger.com>
2014-06-08 12:11:03 +0100

    Restored tuple error messages

--- lib/spree_google_merchant/feed_builder.rb
+++ lib/spree_google_merchant/feed_builder.rb
@@ -84,25 +84,22 @@ module SpreeGoogleMerchant
     end
 
     def validate_record(product)
-      return false if product.images.length == 0 && product.imagesize == 0 rescue true
-      return false if product.google_merchant_title.nil?
-      return false if product.google_merchant_product_category.nil?
-      return false if product.google_merchant_availability.nil?
-      return false if product.google_merchant_price.nil?
-      #return false if product.google_merchant_description.nil?
-      return false if product.google_merchant_brand.nil?
-      return false if product.google_merchant_gtin.nil?
-      return false if product.google_merchant_mpn.nil?
-      #return false if product.google_merchant_shipping_weight.nil?
-      return false unless validate_upc(product.upc)
-
-      unless product.google_merchant_sale_price.nil?
-        return false if product.google_merchant_sale_price_effective.nil?
+      #return false, "Images Invalid" if product.images.length == 0 || product.imagesize == 0
+      return false, "Title Invalid" if product.google_merchant_title.blank?
+      return false, "Category Invalid" if product.google_merchant_product_category.blank?
+      return false, "No Availablity" if product.google_merchant_availability.blank?
+      return false, "Price Invalid" if product.google_merchant_price.blank?
+      return false, "Description Invalid" if product.google_merchant_description.blank?
+      return false, "Brand Invalid" if product.google_merchant_brand.blank?
+      return false, "GTIN Invalid" if product.google_merchant_gtin.blank?
+      return false, "SKU Invalid" if product.google_merchant_mpn.blank?
+      return false, "Shipping Weight Invalid" if product.google_merchant_shipping_weight.blank?
+      return false, "UPC Invalid" unless validate_upc(product.master.gtin)
+      unless product.google_merchant_sale_price.blank?
+        return false, "Invalid sale price" if product.google_merchant_sale_price_effective.blank?
       end
+    end
 
-      true
-    end    
-    
     def generate_xml output
       xml = Builder::XmlMarkup.new(:target => output)
       xml.instruct!
@@ -112,8 +109,12 @@ module SpreeGoogleMerchant
           build_meta(xml)
 
           ar_scope.find_each(:batch_size => 300) do |product|
-            next unless validate_record(product)
-            build_product(xml, product)
+            valid, msg = validate_record(product)
+            if valid
+              build_product(xml, product)
+            else
+              puts "#{product.name} Failed: #{msg}"
+            end
           end
         end
       end
